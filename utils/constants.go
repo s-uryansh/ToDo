27e6234286135151df47b01784b8df8c366cc9d9
Suryansh Rohil <suryanshrohilwork@gmail.com>
2024-10-16 12:34:15 +0530

    constants.go

--- utils/constants.go
+++ utils/constants.go
@@ -2,18 +2,18 @@ package utils
 
 const (
 	SessionDuration                   = 1200
-	SECRET_KEY_TOKEN                  = "a6sdas678das8das98dagds97gasfza"
-	REDIS_KEY_TOKEN                   = "a6sdas678das8sxdcfvgbhndas98dagds97gasfza"
+	SECRET_KEY_TOKEN                  = //Secret Key
+	REDIS_KEY_TOKEN                   = //Redis KEY
 	ZipLength                         = 6
-	FROM_MAIL                         = "gothrowaway28@gmail.com"
-	PASSWORD_MAIL                     = "xfhzzfrjckykcvdf"
+	FROM_MAIL                         = //Mail to send registration mail
+	PASSWORD_MAIL                     = //Password for mail to forward
 	SMTP_ADDRESS                      = "smtp.gmail.com"
 	SMTP_DIAL                         = "smtp.gmail.com:587"
 	REGISTER_TemplatePath             = "../../template/registration_email.html"
 	REGISTER_TemplatePath_SSO         = "../../template/register_sso.html"
 	REGISTER_Success_TemplatePath_SSO = "C:\\Users\\surya\\OneDrive\\Desktop\\Files\\Codes\\Go\\Projects\\CRUD-SQL\\template"
-	CLIENT_ID                         = "16774555392-et2s3a9aim3hb5kok3c7bhjse0it52pb.apps.googleusercontent.com"
-	CLIENT_SECRET                     = "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"
+	CLIENT_ID                         = //Put Client ID here
+	CLIENT_SECRET                     = //Client Secret here
 	REDIRECT_URI                      = "http://localhost:8080/callback"
 	//Email Subjects and bodies
 
